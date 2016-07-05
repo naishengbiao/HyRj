9d14a7c0cf334fc73e25510aa0035eb98715053a
adc.net.nai <adc.net.nai@gmail.com>
2016-07-05 14:04:41 +0800

    No 2

---
+++ HyRj.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HyRj.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Test(int id=0)
+        {
+            ViewBag.d = id;
+            return View();
+        }
+
+    }
+}
